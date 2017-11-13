7cad3650c38ca96003cf17f81961623d37332d36
Zuri Lawrence <zlawrence@gwmail.gwu.edu>
2017-11-12 19:01:58 -0500

    Add lookup() and symhash()

--- Week4/project4.c
+++ Week4/project4.c
@@ -3,7 +3,37 @@
 #include <string.h>
 #include "project_template.h"
 
+int yyparse(void);
 
+static unsigned symhash(char *sym)
+{
+	unsigned int hash = 0;
+	unsigned c;
+
+	while(c = *sym++) hash = hash*9 ^ c;
+
+	return hash;
+}
+
+struct symbol * lookup(char *sym)
+{
+	struct symbol *sp = &symtab[symhash(sym) % NHASH];
+	int scount = NHASH;
+
+	while(--scount >= 0) {
+		if(sp->name && !strcmp(sp->name, sym)) { return sp; }
+
+		if(!sp->name) {
+			sp->name = strdup(sym);
+			sp->value = 0;
+			return sp;
+		}
+
+		if(++sp >= symtab + NHASH) sp = symtab;
+	}
+	yyerror("symbol table overflow\n");
+	abort();
+}
 
 void yyerror(char *s)
 {
